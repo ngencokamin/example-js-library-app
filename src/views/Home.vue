63d0d0f926c41d0332de750782dc20ea20855f79
nix <ngencokamin@gmail.com>
2021-02-22 15:44:17 -0800

    Initial commit, uses sweetalert 2 to make a few popups

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button v-on:click="firstButton()">Click me!</button>
+    <br />
+    <button v-if="secondShow" v-on:click="secondButton()">Don't click this button</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Swal from "sweetalert2";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function() {
+    return {
+      secondShow: false,
+    };
+  },
+  created: function() {
+    Swal.fire("Welcome!", "Please press the button!");
+  },
+  methods: {
+    firstButton: function() {
+      Swal.fire("Congrats, you pressed the button!", "Don't click the next one, though.", "success");
+      this.secondShow = true;
+    },
+    secondButton: function() {
+      Swal.fire("Wow, rude!", "I said not to do it! I could not have been more clear!", "error");
+    },
   },
 };
 </script>
